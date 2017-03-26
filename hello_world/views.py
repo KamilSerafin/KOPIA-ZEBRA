ee0058b8600cdfb1cc89b023a25418f70f94772b
tester <x@x.com>
2017-03-26 12:45:23 +0200

    Travis 1.2

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Marcin"
+moje_imie = "Maciej"
 msg = "Hello World!"
 
 @app.route('/')
